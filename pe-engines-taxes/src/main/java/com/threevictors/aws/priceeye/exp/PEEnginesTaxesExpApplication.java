26bb0b999ed3353385bfe3b0a1630e249517a521
Praneeth JannapuReddy <pjannapureddy@atpco.net>
2025-04-29 06:00:18 -0400

    First commit with a working maven build.

---
+++ pe-engines-taxes/src/main/java/com/threevictors/aws/priceeye/exp/PEEnginesTaxesExpApplication.java
@@ -0,0 +1,22 @@
+package com.threevictors.aws.priceeye.exp;
+
+
+import net.atpco.service.fee.client.request.TaxServiceFeeQuery;
+import net.atpco.taxes.engine.service.TaxService;
+
+public class PEEnginesTaxesExpApplication {
+
+    public static void main(String[] args) {
+        System.out.println("PEEnginesTaxesExpApplication started!");
+
+        /* TODO: First, stub the TaxServiceFeeQuery object and the other booleans as per the required signature on how the TaxesController makes the call.
+            Stub with values examined from the POST request made on local.
+            Print the calculated taxes. Watch out for the currency conversion error issues that occurred on local.
+            If I am getting some successful response, then the plan is to stub a PEItinerary object with some values and transform it to TaxServiceFeeQuery query and make the call.
+            Note that the PEItinerary does not have basefare, so start with a value of 1 or 100 and see how it goes.*/
+        //TaxServiceFeeQuery query = TaxServiceFeeQuery.builder().build();
+        //TaxService taxService = new TaxService();
+
+    }
+
+}
